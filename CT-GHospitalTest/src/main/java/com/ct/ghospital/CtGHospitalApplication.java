ab8dbef4f7f4c20fbc5b1b993621140e5577a43e
Niraj Singh <Nirajkumar.Singh@citiustech.com>
2022-01-08 15:21:08 +0530

    eureka client

--- CT-GHospitalTest/src/main/java/com/ct/ghospital/CtGHospitalApplication.java
+++ CT-GHospitalTest/src/main/java/com/ct/ghospital/CtGHospitalApplication.java
@@ -2,13 +2,22 @@ package com.ct.ghospital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+import org.springframework.web.client.RestTemplate;
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CtGHospitalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CtGHospitalApplication.class, args);
 	}
-
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+		
+	}
 }
